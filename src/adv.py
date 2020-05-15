488a2fb2261e069ddf00a9ca1d3696b3387b35ac
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-05-14 22:08:21 -0400

    crete new player object

--- src/adv.py
+++ src/adv.py
@@ -39,7 +39,9 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player("Leighton", room['outside'])
 
+done = False
 # Write a loop that:
 #
 # * Prints the current room name
