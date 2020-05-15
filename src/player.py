f29c07520056eb0d9591892d1286888be98a401e
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-05-14 22:11:29 -0400

    setup player.py with dunder methods (name, location, items) - used hasattr and getattr

--- src/player.py
+++ src/player.py
@@ -1,2 +1,26 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, location, items=[]):
+        self.name = name
+        self.location = location
+        self.items = items
+
+    def move_to(self, direction, current_loc):
+        # try to move in the specified direction
+        attribute = direction + '_to'
+
+        # if can move in specified direction from current location
+        if hasattr(current_loc, attribute):
+            return getattr(current_loc, attribute)
+
+        print("You CANNOT go that way\n")
+
+        return current_loc
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def remove_item(self, item):
+        self.items.pop(item)
